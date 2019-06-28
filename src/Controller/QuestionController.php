e66c712969a3fc9950dd8ac56c647255716de3c4
Geoffrey BEDLE <geoffrey.bedle@gmail.com>
2019-06-28 09:17:13 +0200

    renvoit des données en json

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -6,21 +6,27 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\SerializerInterface;
 use App\Entity\Question;
 
 
 class QuestionController extends AbstractController
 {
     /**
-     * @Route ("/question/{id}", name="show_question")
+     * @Route ("/question/{position}",defaults={"position"=1}, name="show_question")
      */
-    public function showQuestion(Question $question)
+    public function showQuestion(Question $question, SerializerInterface $serializer)
     {
-        return $this->render('base.html.twig',
-            [
-                'question' => $question,
-                'answers' => $question->getAnswers()
-            ]
-        );
+
+        $data = [];
+        $data[]= $question;
+        $data[]= $question->getAnswers();
+      //  dd($data);
+
+        $jsonData = $serializer->serialize($data,'json');
+        return new Response($jsonData, 200,['Content-Type'=> 'application/json']);
+
+
     }
 }
+
